40c604f29847d3b445af2a61f527c49250a2b6d8
juampyz7 <jlojaquichimbo@gmail.com>
2024-11-07 11:56:01 -0500

    Resolucion del problema 1

--- Proyecto-Taller5/src/Problema1_Edadvotar.java
+++ Proyecto-Taller5/src/Problema1_Edadvotar.java
@@ -1,3 +1,25 @@
+
+import java.util.Scanner;
+
+/*** 
+ *Edad para Votar: Determinar si una persona es elegible para 
+ *votar en función de su edad (mayor o igual a 18 años).
+ * @autor juampyz7
+ */
+
 public class Problema1_Edadvotar {
-    
+    public static void main(String[] args) {
+        int edad;
+        Scanner tcl = new Scanner(System.in);
+        System.out.print("DAME LA EDAD: ");
+        edad = tcl.nextInt();
+        if (edad>=18)
+            System.out.println("SI PUEDE VOTAR");
+        else
+            System.out.println("NO PUEDE VOTAR");
+           
+    }
 }
+/***
+ * run:
+ */
