7f78655128a86168d26dde280ecff3276eeea082
PatitoCorrea <rafaelcorrealopez21@gmail.com>
2024-11-08 18:22:23 -0500

    resolucion del problema 4

---
+++ Proyecto-Taller5/src/Problema4_DescuentoCompras.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+/***
+ *Descuento en Compras: Calcular el precio final de un producto con un descuento del 10% si el precio original es mayor de $100; de lo contrario, no hay descuento.
+ * @author patito
+ */
+public class Problema4_DescuentoCompras {
+    public static void main(String[] args) {
+        double preciofd, preciofinal, precioproducto;
+        Scanner tcl = new Scanner(System.in);
+        System.out.println("INGRESE EL PRECIO DEL PRODUCTO: ");
+        precioproducto = tcl.nextInt();
+        preciofinal = precioproducto;
+        if (preciofinal>100) {
+            preciofinal = precioproducto;
+            System.out.println("EL PRECIO FINAL DEL PRODUCTO ES: " + preciofinal);
+        }
+        else {
+            preciofd = (precioproducto * 0.90);
+            preciofinal = preciofd;
+            System.out.println("EL PRECIO FINAL DEL PRODUCTO ES: " + preciofinal);
+        }
+           
+        
+    }
+    
+}
